070c9244bdc28186729e6bafd7749ccfdff7477a
Christopher Dorst <chris@dorst.email>
2017-12-31 03:09:06 -0600

    Add DirectoryUploader

---
+++ DevOps.ContentDelivery.Functions.UploadDirectoryToAzureBlobContainer/DirectoryUploader.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using static Common.Functions.GetRelativePath.RelativePathGetter;
+using static Common.Functions.ListFilesUnderDirectory.DirectoryFileLister;
+using static DevOps.ContentDelivery.Functions.GetOrCreateAzureBlobContainer.ContainerReferenceGetter;
+using static DevOps.ContentDelivery.Functions.UploadAzureBlob.BlobUploader;
+
+namespace DevOps.ContentDelivery.Functions.UploadDirectoryToAzureBlobContainer
+{
+    public static class DirectoryUploader
+    {
+        public static async Task UploadFiles(string containerName, string path, string connectionString = null)
+        {
+            var container = await Container(containerName, connectionString);
+            foreach (string file in ListFiles(path))
+                await Upload(container,
+                    name: Relative(path, file),
+                    path: file);
+        }
+    }
+}
